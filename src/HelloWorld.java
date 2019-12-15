2b3fae18d19491eaa590d913a97edcda10322856
Wadewhy <513985092@qq.com>
2019-12-15 22:25:38 +0800

    first

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * @PACKAGE_NAME: PACKAGE_NAME
+ * @NAME: HelloWorld
+ * @Author: 钟子豪
+ * @DATE: 2019/12/15
+ * @MONTH_NAME_FULL: 十二月
+ * @DAY: 15
+ * @DAY_NAME_FULL: 星期日
+ * @PROJECT_NAME: Test
+ **/
+public class HelloWorld {
+}
